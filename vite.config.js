17db6a52cb23e66e4e289d379f1a0ffb829526b2
Mohsin Riaz <riaz.mohsin@gmail.com>
2024-02-06 18:29:16 -0500

    Fixed: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/minesweeper/',
     plugins: [react()],
     server: { port: 3000, open: true, host: true },
-})
+});
